0ed12fb94a56356d4af89a72c2fd2579b3b9136d
DELL <DELL@DESKTOP-QH20R7M>
2023-03-23 06:53:55 +0530

    first

--- src/GqtCodes/Program1.java
+++ src/GqtCodes/Program1.java
@@ -4,6 +4,7 @@ public class Program1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello world");
+		System.out.println("Hello");
 	}
 
 }
