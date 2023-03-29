e8f3a1a54b504919c9c5a7ec28f6a15aac8f7d65
DELL <DELL@DESKTOP-QH20R7M>
2023-03-29 07:34:38 +0530

    Narendra

--- src/GqtCodes/GrNarendra.java
+++ src/GqtCodes/GrNarendra.java
@@ -8,6 +8,7 @@ public class GrNarendra {
 		System.out.println("Enter the value:");
 		int n = sc.nextInt();
 		for(int i=0;i<=n-1;i++) {
+			//G
 			for(int j=0;j<=n-1;j++) {
 				if(  (j==0 && i!=0 && i!=(n-1))
 				  || (i==0 && j!=0 && j<=n/2)
