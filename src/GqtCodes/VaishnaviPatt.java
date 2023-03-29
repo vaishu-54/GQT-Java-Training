ae3c1a5fdd5411baa0b1fddb38770e9fba19d11e
DELL <DELL@DESKTOP-QH20R7M>
2023-03-29 07:34:03 +0530

    Vaishnavi pattern

--- src/GqtCodes/VaishnaviPatt.java
+++ src/GqtCodes/VaishnaviPatt.java
@@ -46,7 +46,7 @@ public class VaishnaviPatt {
 			
 			//S
 			for(int j=0;j<=n-1;j++) {
-				if(  (i==0 && j<n/2 && j!=0)
+				if(  (i==0 && j<=n/2 && j!=0)
 				  || (i==n/2 && j<n/2 && j!=0 && j!=n/2)
 				  || (i==(n-1) && j<n/2 && j!=n/2)
 				  || (j==0 && i<n/2 && i!=0)
@@ -79,7 +79,8 @@ public class VaishnaviPatt {
 					System.out.print("  ");
 				}
 			}
-			System.out.print("    ");
+			System.out.print("        ");
+			
 			//A
 			for(int j=0;j<=n-1;j++) {
 				if(j==0 || j==n/2 
@@ -94,8 +95,16 @@ public class VaishnaviPatt {
 			
 			//V
 			for(int j=0;j<=n-1;j++) {
-				if( (i==j && i<=n/2 && j<=n/2)
-				  ||(i+j==(n-1) && i<=n/2 && j>=n/2)
+				if(  i==j
+						) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			for(int j=0;j<=n-1;j++) {
+				if( i+j==(n-1)
 						) {
 					System.out.print("# ");
 				}
