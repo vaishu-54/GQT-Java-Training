a026fc6711c53a92ff1deebaa5d3af0cd32418ae
DELL <DELL@DESKTOP-QH20R7M>
2023-03-29 06:28:49 +0530

    GQT Pattern

--- src/GqtCodes/GqtPatterns.java
+++ src/GqtCodes/GqtPatterns.java
@@ -1,9 +1,72 @@
 package GqtCodes;
-
+import java.util.Scanner;
 public class GqtPatterns {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value:");
+		int n = sc.nextInt();
+		for(int i=0;i<=n-1;i++) {
+			//G
+			for(int j=0;j<=n-1;j++) {
+				if(  (j==0 && i!=0 && i!=(n-1))
+				  || (i==0 && j!=0 && j<=n/2)
+				  || (i==(n-1) && j<n/2  && j!=0 && j!=n/2)
+				  || (i==n/2 && j>n/4 && j<=n/2)
+				  || (j==n/2 && i>=n/2 && i!=(n-1))
+						) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			
+			//Q
+			for(int j=0;j<=n-1;j++) {
+				if(  (j==0 && i!=0 && i!=(3*n)/4 && i<(3*n)/4)
+				  || (j==n/2 && i!=0 && i!=(3*n)/4 && i<(3*n)/4)
+				  || (i==0 && j!=0 && j!=n/2 && j<n/2)
+				  || (i==(3*n)/4 && j!=0 && j!=n/2 && j<n/2)
+				  || i-j==n/2
+						) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			
+			//I
+			for(int j=0;j<=n-1;j++) {
+				if(  (i==0 && j<n/2)
+				  || (j==n/4 )	
+						) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			
+			//S
+			for(int j=0;j<=n-1;j++) {
+				if(  (i==0 && j<n/2 && j!=0)
+				  || (i==n/2 && j<n/2 && j!=0 && j!=n/2)
+				  || (i==(n-1) && j<n/2 && j!=n/2)
+				  || (j==0 && i<n/2 && i!=0)
+				  || (j==n/2 && i>n/2 && i!=(n-1))
+						) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			
+			//H
+			System.out.println();
+		}
 
 	}
 
